1f3381b168e532171941ce39392c16828ee6ffa1
Dennis Wong <denniswong178@gmail.com>
2022-07-12 17:04:46 +0800

    edit

--- array/array.h
+++ array/array.h
@@ -2,10 +2,24 @@
 #include<stdlib.h>
 #include<stdint.h>
 
-#define NUMBER_OF_SAMPLES 5
+//Constants
+#define LUX_THRESHOLD                              400
+#define NUMBER_OF_SAMPLES                          5
+#define INDEX_INITIAL                              0
 
+//Front/Back Light status
+#define LIGHT_STATUS_ON                            0x00
+#define LIGHT_STATUS_OFF                           0x01
+#define LIGHT_STATUS_INITIAL                       0x01
+
+//Light operation mode
+#define LIGHT_MODE_AUTO                            0x00
+#define LIGHT_MODE_OFF                             0x01
+#define LIGHT_MODE_ON                              0x02
+#define LIGHT_MODE_INITIAL                         0x00
 //uint8_t light_on [NUMBER_OF_SAMPLES] = {};
 
 void light_on_Init(void);
 void printing(void);
 void print_light_on();
+void light_status_indicator(uint8_t light_mode);
--- array/func.c
+++ array/func.c
@@ -1,6 +1,7 @@
 #include "array.h"
 
 static uint8_t light_on[NUMBER_OF_SAMPLES] = {}; 
+static uint8_t light_on_initial[NUMBER_OF_SAMPLES] = {}; 
 
 void printing (void)
 {
@@ -12,7 +13,8 @@ void light_on_Init()
 {
     for (int i = 0; i<NUMBER_OF_SAMPLES; i++)
         {
-            light_on[i] = 0;
+            light_on_initial[i] = 0;
+            light_on[i] = light_on_initial[i];
             i++;
         }
 }
@@ -21,8 +23,42 @@ void print_light_on()
 {
     for (int i = 0; i<NUMBER_OF_SAMPLES; i++)
         {
-            printf("%d\n",light_on[i]+i);
-        }    
+            printf("%d\n",light_on[i]);
+            
+        }
+
+    printf("Light on initial\n");
+
+    for (int i = 0; i<NUMBER_OF_SAMPLES; i++)
+        {
+            
+            printf("%d\n",light_on_initial[i]);
+        }     
 }
+
+void light_status_indicator(uint8_t light_mode)
+{
+    switch(light_mode)
+    {
+        case 'LIGHT_MODE_AUTO':
+            printf("Now in auto mode");
+        break;
+
+        case 'LIGHT_MODE_ON':
+            printf("Now in on mode");
+        break;
+
+        case 'LIGHT_MODE_OFF':
+            printf("Now in off mode");
+        break;
+
+        default: 
+            printf("Error! Something not right");
+    }
+    
+}
+
+
+
 
 //:D
